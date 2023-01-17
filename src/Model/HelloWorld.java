dd95e8324aa1b48ce8e2e6cc849933a379d213e9
haoyi he <he.haoyi@northeastern.edu>
2023-01-17 17:58:12 -0500

    Assignment0

---
+++ src/Model/HelloWorld.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class HelloWorld {public static void main(String[] args){
+    int var1=2;
+    double var2=21.2d;
+    float var3=5.75f;
+    String var4="Hello";
+    Boolean var5=true;
+
+   int[] arr=new int[10];
+   arr[0]=1;
+   arr[1]=2;
+   arr[2]=3;
+   arr[3]=4;
+   arr[4]=5;
+   arr[5]=6;
+   arr[6]=9;
+   arr[7]=10;
+   arr[8]=8;
+   arr[9]=4;
+
+
+
+System.out.println(var1);
+System.out.println(var2);
+System.out.println(var3);
+System.out.println(var4);
+System.out.println(var5);
+for (int printArr:arr){
+    System.out.println(printArr);
+}
+
+
+
+
+}
+}
